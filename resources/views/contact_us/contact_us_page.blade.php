825580529d79e433a9cb2704c723746314736e59
Htoo Maung Thait <hmthait@blueplanetmm.com>
2022-06-30 06:31:33 +0630

    banner area updated

--- resources/views/contact_us/contact_us_page.blade.php
+++ resources/views/contact_us/contact_us_page.blade.php
@@ -1,8 +1,19 @@
 @extends('layout.master')
 
 @section('page_content')
-     <!--================Slider Area =================-->
-     @include('landing.main_slider')
+     <!--================Banner Area =================-->
+     <section class="banner_area">
+          <div class="container">
+          <div class="banner_inner_content">
+               <h3>Contact Us</h3>
+               <ul>
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li><a href="about-us.html">Contact Us</a></li>
+               </ul>
+          </div>
+          </div>
+     </section>
+     <!--================End Banner Area =================-->
 
      <!--================Introduction Area =================-->
      @include('landing.introduction')
