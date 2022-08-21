81bed1a21691961b8ba8f4941f6935897e918563
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-08-21 15:42:05 +0630

    mangrove view resort page logo update

--- resources/views/resort/resort_banner_area.blade.php
+++ resources/views/resort/resort_banner_area.blade.php
@@ -1,15 +1,35 @@
 <section class="banner_area"
-style="background: url({{asset('img/banner/resort_banner_bg.jpg')}}) no-repeat center; background-size: cover;"
->
+    style="background: url({{ asset('img/banner/resort_banner_bg.jpg') }}) no-repeat center; background-size: cover;">
     <div class="container">
-        <div class="banner_inner_content"
+        <div class="banner_inner_content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2 col-md-5 col-sm-5 col-xs-7" >
+                        <img
+                        width="80%"
+                        src="{{ asset('images/logos/mangrove_view_logo_lemon_chiffon_color.png') }}" />
+                    </div>
+                    <div
+                        class="col-lg-10 col-sm-12 col-xs-12 mangrove_view_resort_banner_text"
+                        style="margin-top: 32px;"
+                        >
+                        <h3>Mangrove View Resort</h3>
+                    </div>
 
-        >
-            <h3>Resort</h3>
-            <ul>
-                <li ><a href="{{route('home.landing_page')}}">Home</a></li>
-                <li class="active"><a href="{{route('page.resort')}}">Resort</a></li>
-            </ul>
+
+
+                </div>
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <ul>
+                            <li><a href="{{ route('home.landing_page') }}">Home</a></li>
+                            <li class="active"><a href="{{ route('page.resort') }}">Resort</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </section>
