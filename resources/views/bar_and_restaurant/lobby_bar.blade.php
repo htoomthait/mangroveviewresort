29b3ad985b6abf08c8bd96e9182cbbe6d343953c
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-09-30 12:02:18 +0630

    lobby bar items updated

--- resources/views/bar_and_restaurant/lobby_bar.blade.php
+++ resources/views/bar_and_restaurant/lobby_bar.blade.php
@@ -16,7 +16,7 @@
                                 <i class="fa fa-expand"
                                     style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
                                     aria-hidden="true"></i>
-                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_1_thumbnail.jpg') }}" alt="guide map">
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_1_thumbnail.jpg') }}" alt="lobby item 1">
                             </a>
                         </div>
                         <h4>Item 1</h4>
@@ -32,7 +32,7 @@
                                 <i class="fa fa-expand"
                                     style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
                                     aria-hidden="true"></i>
-                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_2_thumbnail.jpg') }}" alt="guide map">
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_2_thumbnail.jpg') }}" alt="lobby item 2">
                             </a>
                         </div>
                         <h4>Item 2</h4>
@@ -46,7 +46,7 @@
                                 <i class="fa fa-expand"
                                     style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
                                     aria-hidden="true"></i>
-                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_3_thumbnail.jpg') }}" alt="guide map">
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_3_thumbnail.jpg') }}" alt="lobby item 3">
                             </a>
                         </div>
                         <h4>Item 3</h4>
@@ -60,10 +60,67 @@
                                 <i class="fa fa-expand"
                                     style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
                                     aria-hidden="true"></i>
-                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_4_thumbnail.jpg') }}" alt="guide map">
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_4_thumbnail.jpg') }}" alt="lobby item 4">
                             </a>
                         </div>
                         <h4>Item 4</h4>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="special_dish_item text-center">
+                        <div class="dish_img">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_5.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 5">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_5_thumbnail.jpg') }}" alt="lobby item 5">
+                            </a>
+                        </div>
+                        <h4>Item 5</h4>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="special_dish_item text-center">
+                        <div class="dish_img">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_6.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 6">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_6_thumbnail.jpg') }}" alt="lobby item 6">
+                            </a>
+                        </div>
+                        <h4>Item 6</h4>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="special_dish_item text-center">
+                        <div class="dish_img">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_7.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 7">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_7_thumbnail.jpg') }}" alt="lobby item 7">
+                            </a>
+                        </div>
+                        <h4>Item 7</h4>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-xs-6">
+                    <div class="special_dish_item text-center">
+                        <div class="dish_img">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_8.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 8">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_8_thumbnail.jpg') }}" alt="lobby item 8">
+                            </a>
+                        </div>
+                        <h4>Item 8</h4>
                     </div>
                 </div>
             </div>
