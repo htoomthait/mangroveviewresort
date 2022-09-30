3cfb5bffb00f64df2f974747b54e705858591843
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-09-30 11:46:50 +0630

    lobby bar some items's image updated

--- resources/views/bar_and_restaurant/lobby_bar.blade.php
+++ resources/views/bar_and_restaurant/lobby_bar.blade.php
@@ -1,45 +1,69 @@
 <section class="special_dish_area" id="lobby_bar">
     <div class="container">
         <div class="special_dish_inner_area">
-            <div class="main_big_title" >
-                <h2 >Our Lobby <span>bar</span></h2>
-                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise</p>
+            <div class="main_big_title">
+                <h2>Our Lobby <span>bar</span></h2>
+                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is
+                    pain, but because occasionally circumstances occur in which toil and pain can procure him some great
+                    pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise</p>
             </div>
             <div class="row special_dish_inner">
                 <div class="col-md-3 col-xs-6">
-                    <div class="special_dish_item">
+                    <div class="special_dish_item text-center">
                         <div class="dish_img">
-                            <img src="img/restaurant/dish-1.jpg" alt="">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_1.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 1">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_1_thumbnail.jpg') }}" alt="guide map">
+                            </a>
                         </div>
-                        <a href="#"><h4>bakery items</h4></a>
-                        <p>Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed do eiusmod ipsum dolor sit amet</p>
+                        <h4>Item 1</h4>
+
+
                     </div>
                 </div>
                 <div class="col-md-3 col-xs-6">
-                    <div class="special_dish_item">
+                    <div class="special_dish_item text-center">
                         <div class="dish_img">
-                            <img src="img/restaurant/dish-2.jpg" alt="">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_2.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 2">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_2_thumbnail.jpg') }}" alt="guide map">
+                            </a>
                         </div>
-                        <a href="#"><h4>bakery items</h4></a>
-                        <p>Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed do eiusmod ipsum dolor sit amet</p>
+                        <h4>Item 2</h4>
                     </div>
                 </div>
                 <div class="col-md-3 col-xs-6">
-                    <div class="special_dish_item">
+                    <div class="special_dish_item text-center">
                         <div class="dish_img">
-                            <img src="img/restaurant/dish-3.jpg" alt="">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_3.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 3">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_3_thumbnail.jpg') }}" alt="guide map">
+                            </a>
                         </div>
-                        <a href="#"><h4>bakery items</h4></a>
-                        <p>Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed do eiusmod ipsum dolor sit amet</p>
+                        <h4>Item 3</h4>
                     </div>
                 </div>
                 <div class="col-md-3 col-xs-6">
-                    <div class="special_dish_item">
+                    <div class="special_dish_item text-center">
                         <div class="dish_img">
-                            <img src="img/restaurant/dish-4.jpg" alt="">
+                            <a href="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_4.jpg') }}"
+                                data-lightbox="lobby_bar" data-title="Item 4">
+                                <i class="fa fa-expand"
+                                    style="position: absolute; z-index:100; margin-top:8px; left: 88%; font-size:21px;"
+                                    aria-hidden="true"></i>
+                                <img src="{{ asset('images/restaurant/lobby_bar/lobby_bar_item_4_thumbnail.jpg') }}" alt="guide map">
+                            </a>
                         </div>
-                        <a href="#"><h4>bakery items</h4></a>
-                        <p>Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed do eiusmod ipsum dolor sit amet</p>
+                        <h4>Item 4</h4>
                     </div>
                 </div>
             </div>
