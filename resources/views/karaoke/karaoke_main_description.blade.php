5e5bafb8b04b584dbdca45469289b52c99261e72
Htoo Maung Thait <htoomaungthait@gmail.com>
2022-11-19 17:16:33 +0630

    karaoke page layout updated

--- resources/views/karaoke/karaoke_main_description.blade.php
+++ resources/views/karaoke/karaoke_main_description.blade.php
@@ -1,42 +1,15 @@
 <section class="introduction_area resort_story_area " id="sec_karaoke" style="padding-bottom:52px;">
     <div class="container resort-block-container">
         <div class="left_ex_title">
-            <h2>our <span>karaokie</span></h2>
-            <p></p>
-            <p></p>
-        </div>
-        <div class="row introduction_inner">
-            <div class="col-md-5">
-
-                <a
-                    class="introduction_img"
-                    href="{{ asset('images/resort/karaoke_1.jpg') }}"
-                    data-lightbox="karaoke_page" data-title="Karaoke Room Dark Theme">
-                    <i class="fa fa-expand about_us_graden_expand" style="" aria-hidden="true"></i>
-                    <img class="img-fluid" width="100%" src="{{ asset('images/resort/karaoke_1_thumbnail.jpg') }}"
-                        alt="guide map" />
-                </a>
-
+            <h2>our resort <span>karaokie</span></h2>
+            <div class="resort_title">
+                <h2></h2>
+                <h5>give best service to our customers</h5>
             </div>
-            <div class="col-md-7">
-                <div class="introduction_left_text">
-                    <div class="resort_title">
-                        <h2>our resort <span>karaokie</span></h2>
-                        <h5>give best service to our customers</h5>
-                    </div>
-                    <h6>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</h6>
-                    <h4>We are Available for business</h4>
-                    <p>quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-                        voluptatem. Ut enim ad minima veniam. quis nostrum exercitationem ullam corporis suscipit
-                        laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui
-                        in ea volup.</p>
-                    <p>Ut enim ad minima veniam. quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-                        aliquid ex ea commodi consequatut.</p>
-                    {{-- <a class="about_btn_b" href="#">contact us</a> --}}
-                </div>
-            </div>
+
         </div>
-        <div class="row mt-4">
+        <div class="row">
+
             <div class="col-md-6 col-sm-12 mt-4">
                 <a
                 class="introduction_img"
@@ -59,5 +32,36 @@
 
             </div>
         </div>
+        <p></p>
+        <p></p>
+        <div class="row introduction_inner mt-4">
+            {{-- <div class="col-md-5">
+
+                <a
+                    class="introduction_img"
+                    href="{{ asset('images/resort/karaoke_1.jpg') }}"
+                    data-lightbox="karaoke_page" data-title="Karaoke Room Dark Theme">
+                    <i class="fa fa-expand about_us_graden_expand" style="" aria-hidden="true"></i>
+                    <img class="img-fluid" width="100%" src="{{ asset('images/resort/karaoke_1_thumbnail.jpg') }}"
+                        alt="guide map" />
+                </a>
+
+            </div> --}}
+            <div class="col-md-12 mt-4">
+                <div class="introduction_left_text">
+
+                    <h6>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</h6>
+                    <h4>We are Available for business</h4>
+                    <p>quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                        voluptatem. Ut enim ad minima veniam. quis nostrum exercitationem ullam corporis suscipit
+                        laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui
+                        in ea volup.</p>
+                    <p>Ut enim ad minima veniam. quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+                        aliquid ex ea commodi consequatut.</p>
+                    {{-- <a class="about_btn_b" href="#">contact us</a> --}}
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
