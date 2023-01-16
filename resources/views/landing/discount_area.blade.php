9ac8a1f8f37abcd0f044f9b1eab846372c96db95
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-01-16 11:29:33 +0630

    discount fix update

--- resources/views/landing/discount_area.blade.php
+++ resources/views/landing/discount_area.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-8">
                 <div class="discount_right_content">
                     <h5>Hot offer</h5>
-                    <h4>up to 30% <span>off</span> <br /> in August 2022</h4>
+                    <h4>up to 20% <span>off</span> <br /> in week days!</h4>
                     <a class="book_now_btn" href="{{route('page.contact_us')}}">Contact Us</a>
                 </div>
             </div>
