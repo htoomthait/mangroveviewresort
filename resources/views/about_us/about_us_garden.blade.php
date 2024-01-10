f8d485138f7782769a503a5688f815503924e7cc
Htoo Maung Thait <hmthait@blueplanetmm.com>
2024-01-10 11:34:00 +0630

    update garden area 80 to 300

--- resources/views/about_us/about_us_garden.blade.php
+++ resources/views/about_us/about_us_garden.blade.php
@@ -45,15 +45,9 @@
 
                 <a href="{{ asset('images/entrance/garden_about_us_large.jpg') }}"
                     data-lightbox="garden_guide_map_landing_page" data-title="Garden Entrance">
-                    <i class="fa fa-expand about_us_graden_expand"
-                        style=""
-                        aria-hidden="true"></i>
-                    <img
-                        class="img-fluid"
-                        width="100%"
-                        src="{{ asset('images/entrance/garden_about_us_small.jpg') }}"
-                        alt="guide map"
-                    />
+                    <i class="fa fa-expand about_us_graden_expand" style="" aria-hidden="true"></i>
+                    <img class="img-fluid" width="100%" src="{{ asset('images/entrance/garden_about_us_small.jpg') }}"
+                        alt="guide map" />
                 </a>
             </div>
             <div class="col-md-7">
@@ -65,7 +59,12 @@
                     {{-- <h6>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</h6> --}}
                     {{-- <h4>We are Available for business</h4> --}}
                     <p class="mt-6">
-                        Sea and lake garden spans 80 acres with a glistening blue lake at the heart of it all. The garden boasts a variety of tropical trees bearing seasonal fruits and flowers. The winding paths can be navigated by foot or visitors can choose to travel leisurely by renting bicycles and electric buggies to enjoy the many charms of the gardens. Undertake an array of activities suitable for different ages. It is a place where you can reconnect with nature and revel in all the beauty it has to offer.
+                        Sea and lake garden spans 300 acres with a glistening blue lake at the heart of it all. The
+                        garden boasts a variety of tropical trees bearing seasonal fruits and flowers. The winding paths
+                        can be navigated by foot or visitors can choose to travel leisurely by renting bicycles and
+                        electric buggies to enjoy the many charms of the gardens. Undertake an array of activities
+                        suitable for different ages. It is a place where you can reconnect with nature and revel in all
+                        the beauty it has to offer.
                     </p>
 
 
@@ -79,5 +78,3 @@
         </div>
     </div>
 </section>
-
-
